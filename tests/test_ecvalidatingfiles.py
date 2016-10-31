c77c6e1f6ace2615cf7cb7984016da3adad26cbb
calarose <camille.larose@canada.ca>
2016-10-31 09:05:39 -0600

    Import fixes

--- tests/test_ecvalidatingfiles.py
+++ tests/test_ecvalidatingfiles.py
@@ -4,7 +4,7 @@ import os.path
 import pytest
 import json
 
-from ectyper.src.ecvalidatingfiles import *
+from ecoli_serotyping.src.ecvalidatingfiles import *
 
 
 SCRIPT_DIRECTORY = os.path.dirname(os.path.abspath(__file__)) + "/"
