9229db298c97ef04435ad41d6679921a9f03c56a
Chad Laing <chadlaing@gmail.com>
2017-03-16 13:28:43 -0600

    Python3

--- src/ectyper.py
+++ src/ectyper.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+
+import logging
+
+
 # TEMP_SCRIPT_DIRECTORY = os.path.dirname(os.path.abspath(__file__)) + "/"
 # sys.path.append(os.path.abspath(TEMP_SCRIPT_DIRECTORY + '../'))
 #
@@ -11,56 +16,58 @@
 
 def runProgram():
     """
-    Wrapper for both the serotypeing and virulence finder
+    Wrapper for both the serotyping and virulence finder
     :return: success or failure
     """
+
 
     # createDirs()
     args = parseCommandLine()
 
-    if args.input == None:
-        logging.info('No inputs were given.')
-        print 'Error'
-
-    else:
-        roughGenomesList = getFilesList(args.input)
-        genomesList = checkFiles(roughGenomesList)
-
-        if isinstance(genomesList, list):
-            setGlobalDicts()
-            clearGlobalDicts()
-
-            if initializeDB() == 0:
-                results_file = runBlastQuery(genomesList, 'ECTyperDB')
-                genomesDict = parseResults(results_file)
-                predictionDict = filterPredictions(genomesDict,
-                                                   args.percentIdentity,
-                                                   args.percentLength)
-                matchDict = sortMatches(predictionDict)
-                topMatches = findTopMatches(matchDict)
-
-                logging.info('Top matches are ' + str(topMatches))
-                json_results = formatResults(topMatches, args.verbose)
-
-                if TEST == True:
-                    compareResults(roughGenomesList, topMatches)
-
-                if args.galaxy == 1:
-                    if args.outputFormat == 'json':
-                        json.dump(json_results, args.output)
-                    else:
-                        toGalaxyCSV(json_results, args.verbose, args.output)
-                else:
-                    if args.csv == 1:
-                        toCSV(json_results, args.verbose)
-
-                    print json_results
-                    logging.info('Successfully ended the program.')
-
-
-            else:
-                logging.error(
-                    'There was an error while generating the database.')
-                print 'Error'
-        else:
-            print genomesList
+    # if args.input == None:
+    #     logging.info('No inputs were given.')
+    #     print 'Error'
+    #
+    # else:
+    #     roughGenomesList = getFilesList(args.input)
+    #     genomesList = checkFiles(roughGenomesList)
+    #
+    #     if isinstance(genomesList, list):
+    #         setGlobalDicts()
+    #         clearGlobalDicts()
+    #
+    #         if initializeDB() == 0:
+    #             results_file = runBlastQuery(genomesList, 'ECTyperDB')
+    #             genomesDict = parseResults(results_file)
+    #             predictionDict = filterPredictions(genomesDict,
+    #                                                args.percentIdentity,
+    #                                                args.percentLength)
+    #             matchDict = sortMatches(predictionDict)
+    #             topMatches = findTopMatches(matchDict)
+    #
+    #             logging.info('Top matches are ' + str(topMatches))
+    #             json_results = formatResults(topMatches, args.verbose)
+    #
+    #             if TEST == True:
+    #                 compareResults(roughGenomesList, topMatches)
+    #
+    #             if args.galaxy == 1:
+    #                 if args.outputFormat == 'json':
+    #                     json.dump(json_results, args.output)
+    #                 else:
+    #                     toGalaxyCSV(json_results, args.verbose, args.output)
+    #             else:
+    #                 if args.csv == 1:
+    #                     toCSV(json_results, args.verbose)
+    #
+    #                 print json_results
+    #                 logging.info('Successfully ended the program.')
+    #
+    #
+    #         else:
+    #             logging.error(
+    #                 'There was an error while generating the database.')
+    #             print 'Error'
+    #     else:
+    #         print genomesList
+    logging.print("Done")
