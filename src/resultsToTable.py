f6c0523d5179dcf5baf40adc9db389ffe92781d2
Chad Laing <chadlaing@inoutbox.com>
2017-05-12 15:34:44 -0600

    ADD: Long format does not require all VFs to be known beforehand.

--- src/resultsToTable.py
+++ src/resultsToTable.py
@@ -19,44 +19,38 @@ def results_dict_to_table(list_of_files, list_of_genomes, results_dict):
     """
 
     output_aoa = [["filename"
+                  ,"genome"
                   ,"contig ID"
                   ,"analysis"
                   ,"hit"
+                  ,"description"
                   ,"orientation"
                   ,"start"
-                  ,"stop"
-                  ,"cutoff"]]
-
-    all_vfs_in_analysis = []
-    for g in results_dict:
-        for vf in results_dict[g]["vf"]:
-            all_vfs_in_analysis.append(vf)
-            set(all_vfs_in_analysis)
+                  ,"stop"]]
 
     for f, g in zip(list_of_files, list_of_genomes):
-        serotype = results_dict[g]["otype"]["ant_number"] + ":" +  results_dict[g]["htype"]["ant_number"]
-        serotype_line = [f,"NA","Serotype", serotype, "NA", "NA", "NA", "NA"]
-        output_aoa.append(serotype_line)
 
-        for vf in all_vfs_in_analysis:
-            contig_id = "NA"
+        if "otype" in results_dict[g]:
+            serotype = results_dict[g]["otype"]["ant_number"] + ":" +  results_dict[g]["htype"]["ant_number"]
+            serotype_line = [f, g, "NA","Serotype", serotype, "NA", "NA", "NA", "NA"]
+            output_aoa.append(serotype_line)
+
+        if "vf" not in results_dict[g]:
+            log.warning("No VFs for genome: " + g)
+            continue
+
+        for vf in results_dict[g]["vf"]:
             analysis = "Virulence Factor"
-            hit = "NA"
-            description = "NA"
-            orientation = "NA"
-            startbp = "NA"
-            stopbp = "NA"
-
-            if vf in results_dict[g]["vf"]:
-                br = results_dict[g]["vf"][vf]["blast_record"]
-                contig_id = br["sseqid"]
-                hit = vf
-                description = results_dict[g]["vf"][vf]["description"]
-                orientation = br["sframe"]
-                startbp = br["sstart"]
-                stopbp = br["send"]
+            br = results_dict[g]["vf"][vf]["blast_record"]
+            contig_id = br["sseqid"]
+            hit = vf
+            description = results_dict[g]["vf"][vf]["description"]
+            orientation = br["sframe"]
+            startbp = br["sstart"]
+            stopbp = br["send"]
 
             next_line = [f
+                         ,g
                          ,contig_id
                          ,analysis
                          ,hit
