da1f73380436623019e249cf5ddefc15818630d2
James Hopkins <jamesedhopkins@gmail.com>
2017-05-15 10:00:22 -0600

    "ADD: test for src.blastFunctions.test_create_blastdb()"

--- tests/blastFunctions_test.py
+++ tests/blastFunctions_test.py
@@ -1,5 +1,9 @@
 import src.blastFunctions
 from argparse import Namespace
+from definitions import ROOT_DIR
+import tempfile
+
+tempfile = tempfile.gettempdir()
 
 
 CONST_CSV = False
@@ -26,6 +30,12 @@ TEST_RECORD_FI = {"sstart": "2856508", "length": "742", "qseqid": "wzy_199_AKLQ0
 #set length of match to 0
 TEST_RECORD_FL = {"sstart": "2856508", "length": "0", "qseqid": "wzy_199_AKLQ01000042_O157", "send": "2855767", "sframe": "1", "pident": "99.87", "qlen": "741", "sseqid": "AE005174.2"}
 
+TEST_LIST = [ROOT_DIR + '/Data/test_genomes/GCA_000010745.1_ASM1074v1_genomic.fna',
+             ROOT_DIR + '/Data/test_genomes/GCA_000017745.1_ASM1774v1_genomic.fna',
+             ROOT_DIR + '/Data/test_genomes/GCA_000025165.1_ASM2516v1_genomic.fna',
+             ROOT_DIR + '/Data/test_genomes/GCA_000026545.1_ASM2654v1_genomic.fna',
+             ROOT_DIR + '/Data/test_genomes/GCA_000091005.1_ASM9100v1_genomic.fna']
+
 def test_record_passes_cutoffs():
     assert src.blastFunctions.record_passes_cutoffs(TEST_RECORD, args) == True
     assert src.blastFunctions.record_passes_cutoffs(TEST_RECORD, args_fi) == False
@@ -34,4 +44,6 @@ def test_record_passes_cutoffs():
     assert src.blastFunctions.record_passes_cutoffs(TEST_RECORD_FL, args) == False
 
 
+def test_create_blast_db():
+    assert src.blastFunctions.create_blast_db(TEST_LIST) == tempfile + '/' + 'ectyper_blastdb'
 
