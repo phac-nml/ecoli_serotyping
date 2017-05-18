d493bc0323e0c5af2a97210305212dc8cac53796
Chad Laing <chadlaing@inoutbox.com>
2017-05-18 09:40:31 -0600

    REFORMAT: PEP8

--- tests/blastFunctions_test.py
+++ tests/blastFunctions_test.py
@@ -7,7 +7,6 @@ import hashlib
 
 tempfile = tempfile.gettempdir()
 
-
 CONST_CSV = False
 CONST_INPUT = 'filename'
 CONST_MINGENOMES = 1
@@ -18,37 +17,62 @@ CONST_PERLEN_F = 101
 CONST_SER = True
 CONST_VIR = True
 
-args = Namespace(csv = CONST_CSV, input = CONST_INPUT, minimumGenomes = CONST_MINGENOMES, percentIdentity =CONST_PERIDENT, percentLength = CONST_PERLEN, serotyper = CONST_SER,  virulenceFactors = CONST_VIR)
-#create args that have impossible percent Identity
-args_fi = Namespace(csv = CONST_CSV, input = CONST_INPUT, minimumGenomes = CONST_MINGENOMES, percentIdentity =CONST_PERIDENT_F, percentLength = CONST_PERLEN, serotyper = CONST_SER,  virulenceFactors = CONST_VIR)
-#create args that have imossible percent length
-args_fl = Namespace(csv = CONST_CSV, input = CONST_INPUT, minimumGenomes = CONST_MINGENOMES, percentIdentity =CONST_PERIDENT, percentLength = CONST_PERLEN_F, serotyper = CONST_SER,  virulenceFactors = CONST_VIR)
+args = Namespace(csv=CONST_CSV, input=CONST_INPUT,
+                 minimumGenomes=CONST_MINGENOMES,
+                 percentIdentity=CONST_PERIDENT, percentLength=CONST_PERLEN,
+                 serotyper=CONST_SER, virulenceFactors=CONST_VIR)
+# create args that have impossible percent Identity
+args_fi = Namespace(csv=CONST_CSV, input=CONST_INPUT,
+                    minimumGenomes=CONST_MINGENOMES,
+                    percentIdentity=CONST_PERIDENT_F,
+                    percentLength=CONST_PERLEN, serotyper=CONST_SER,
+                    virulenceFactors=CONST_VIR)
+# create args that have imossible percent length
+args_fl = Namespace(csv=CONST_CSV, input=CONST_INPUT,
+                    minimumGenomes=CONST_MINGENOMES,
+                    percentIdentity=CONST_PERIDENT,
+                    percentLength=CONST_PERLEN_F, serotyper=CONST_SER,
+                    virulenceFactors=CONST_VIR)
 
+# test record we know should meet cutoff
+TEST_RECORD = {"sstart": "2856508", "length": "742",
+               "qseqid": "wzy_199_AKLQ01000042_O157", "send": "2855767",
+               "sframe": "1", "pident": "99.87", "qlen": "741",
+               "sseqid": "AE005174.2"}
+# set percent identity of match to 0
+TEST_RECORD_FI = {"sstart": "2856508", "length": "742",
+                  "qseqid": "wzy_199_AKLQ01000042_O157", "send": "2855767",
+                  "sframe": "1", "pident": "0", "qlen": "741",
+                  "sseqid": "AE005174.2"}
+# set length of match to 0
+TEST_RECORD_FL = {"sstart": "2856508", "length": "0",
+                  "qseqid": "wzy_199_AKLQ01000042_O157", "send": "2855767",
+                  "sframe": "1", "pident": "99.87", "qlen": "741",
+                  "sseqid": "AE005174.2"}
 
-#test record we know should meet cutoff
-TEST_RECORD = {"sstart": "2856508", "length": "742", "qseqid": "wzy_199_AKLQ01000042_O157", "send": "2855767", "sframe": "1", "pident": "99.87", "qlen": "741", "sseqid": "AE005174.2"}
-#set percent identity of match to 0
-TEST_RECORD_FI = {"sstart": "2856508", "length": "742", "qseqid": "wzy_199_AKLQ01000042_O157", "send": "2855767", "sframe": "1", "pident": "0", "qlen": "741", "sseqid": "AE005174.2"}
-#set length of match to 0
-TEST_RECORD_FL = {"sstart": "2856508", "length": "0", "qseqid": "wzy_199_AKLQ01000042_O157", "send": "2855767", "sframe": "1", "pident": "99.87", "qlen": "741", "sseqid": "AE005174.2"}
-
-TEST_LIST = [ROOT_DIR + '/Data/test_genomes/GCA_000010745.1_ASM1074v1_genomic.fna',
-             ROOT_DIR + '/Data/test_genomes/GCA_000017745.1_ASM1774v1_genomic.fna',
-             ROOT_DIR + '/Data/test_genomes/GCA_000025165.1_ASM2516v1_genomic.fna',
-             ROOT_DIR + '/Data/test_genomes/GCA_000026545.1_ASM2654v1_genomic.fna',
-             ROOT_DIR + '/Data/test_genomes/GCA_000091005.1_ASM9100v1_genomic.fna']
+TEST_LIST = [
+    ROOT_DIR + '/Data/test_genomes/GCA_000010745.1_ASM1074v1_genomic.fna',
+    ROOT_DIR + '/Data/test_genomes/GCA_000017745.1_ASM1774v1_genomic.fna',
+    ROOT_DIR + '/Data/test_genomes/GCA_000025165.1_ASM2516v1_genomic.fna',
+    ROOT_DIR + '/Data/test_genomes/GCA_000026545.1_ASM2654v1_genomic.fna',
+    ROOT_DIR + '/Data/test_genomes/GCA_000091005.1_ASM9100v1_genomic.fna']
 
 TEST_QUERIES = [definitions.SEROTYPE_FILE, definitions.VF_FILE]
 
 TEST_DB = ROOT_DIR + '/Data/test_blastdb/ectyper_blastdb'
 TEMP_DB = '/tmp/ectyper_blastdb'
 
+
 def test_record_passes_cutoffs():
     assert src.blastFunctions.record_passes_cutoffs(TEST_RECORD, args) == True
-    assert src.blastFunctions.record_passes_cutoffs(TEST_RECORD, args_fi) == False
-    assert src.blastFunctions.record_passes_cutoffs(TEST_RECORD, args_fl) == False
-    assert src.blastFunctions.record_passes_cutoffs(TEST_RECORD_FI, args) == False
-    assert src.blastFunctions.record_passes_cutoffs(TEST_RECORD_FL, args) == False
+    assert src.blastFunctions.record_passes_cutoffs(TEST_RECORD,
+                                                    args_fi) == False
+    assert src.blastFunctions.record_passes_cutoffs(TEST_RECORD,
+                                                    args_fl) == False
+    assert src.blastFunctions.record_passes_cutoffs(TEST_RECORD_FI,
+                                                    args) == False
+    assert src.blastFunctions.record_passes_cutoffs(TEST_RECORD_FL,
+                                                    args) == False
 
 
 def test_create_blast_db():
@@ -70,7 +94,6 @@ def test_create_blast_db():
         data = file.read()
         s1 = hashlib.sha1(data).hexdigest()
         assert s1 == 'f26f99e51f8f4f0dbb53cb47c3bdb2ed79ed8a30'
-
 
 # def test_run_blast():
 #     assert src.blastFunctions.run_blast(TEST_QUERIES[0], TEMP_DB) == TEMP_DB + '.output'
