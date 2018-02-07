f73c4d5ee8e6e4e7bb47212f6bf52596bcf9bbc5
Kevin Le <kevin.kent.le@gmail.com>
2018-02-06 20:56:34 -0500

    FIX: anaconda has an old version of subprocess32 without the run()

--- ectyper/subprocess_util.py
+++ ectyper/subprocess_util.py
@@ -8,7 +8,6 @@ from __future__ import absolute_import
 from future import standard_library
 standard_library.install_aliases()
 import logging
-import subprocess
 import timeit
 import os
 import sys
