b47aead8823dd3cc4fc753bc329df35392683275
Chad Laing <chadlaing@inoutbox.com>
2018-06-06 12:17:10 -0600

    Integration as one file; unit as separate

--- test/test_ectyper_integration.py
+++ test/test_ectyper_integration.py
@@ -1,19 +1,64 @@
+import sys
 import pytest
+import os
+# sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+# from ectyper import ectyper
 from ectyper import ectyper
 
+def set_input(input, percent_iden=None, output=None):
+    """
+    Create the sys.argv[] without need for commandline input.
+    :param input: Input file given by testing function
+    :param percent_iden: Percent identity for comparison
+    :param output: Location of output
+    :return: None
+    """
+    args = ['-i', input]
+    if percent_iden:
+        args += ['-d', str(percent_iden)]
+    if output:
+        args += ['-o', output]
+    sys.argv[1:] = args
+
+
+def test_integration_invalid_file():
+    """
+    Giving a non-fasta file in fasta-file name.
+    :return: None
+    """
+    file = 'test/Data/test_dir/badfasta.fasta'
+    set_input(file)
+    with pytest.raises(SystemExit) as se:
+        ectyper.run_program()
+    assert se.type == SystemExit
+    assert se.value.code == "No valid genomes"
+
+
+def test_integration_no_file():
+    """
+    Giving no input to the program.
+    :return: None
+    """
+    file = ''
+    set_input(file)
+    with pytest.raises(SystemExit) as se:
+        ectyper.run_program()
+    assert se.type == SystemExit
+    assert se.value.code == "No valid genomes"
+
+
+def test_valid_fastq_file():
+    """
+    Given a valid fastq file, get the correct results.
+    Use a temp dir for the test output
+    :return: None
+    """
+    file = 'test/Data/Escherichia.fastq'
+    set_input(input, output=output_dir)
+    ectyper.run_program()
 
 
 
-
-# def test_valid_fastq_file():
-#     """
-#     Given a valid fastq file, get the correct results.
-#     Use a temp dir for the test output
-#     :return: None
-#     """
-#     file = 'test/Data/Escherichia.fastq'
-#     set_input(input, output=output_dir)
-#     ectyper.run_program()
 
 #     def test_folder_input(self):
 #         input = 'test/Data/test_dir'
@@ -51,7 +96,3 @@ from ectyper import ectyper
 #                          'output_file': os.path.abspath('output/test/output.csv')}
 #         self.assertDictEqual(expected_dict, ectyper.create_tmp_files(
 #             'test/temp', output_dir='test'))
-#
-#
-# if __name__ == '__main__':
-#     unittest.main()
