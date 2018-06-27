25f3732b9b5bd87d592cac08285f9fdd06d537fd
Chad Laing <chadlaing@inoutbox.com>
2018-06-27 14:37:34 -0600

    Checking output of all tests

--- test/test_ectyper_integration.py
+++ test/test_ectyper_integration.py
@@ -1,7 +1,6 @@
 import sys
 import pytest
 import tempfile
-import re
 import os
 from ectyper import ectyper
 
@@ -23,7 +22,7 @@ def set_input(input, percent_iden=None, output=tempfile.mkdtemp()):
     sys.argv[1:] = args
 
 
-def test_integration_invalid_file():
+def test_integration_invalid_file(caplog):
     """
     Giving a non-fasta file in fasta-file name.
     :return: None
@@ -31,6 +30,7 @@ def test_integration_invalid_file():
     file = os.path.join(TEST_ROOT, 'Data/test_dir/badfasta.fasta')
     set_input(file)
     ectyper.run_program()
+    assert "badfasta\tNon fasta / fastq file" in caplog.text
 
 
 def test_integration_no_file():
@@ -46,7 +46,7 @@ def test_integration_no_file():
     assert se.value.code == "No files were found"
 
 
-def test_integration_valid_file():
+def test_integration_valid_file(caplog):
     """
     Ensure a valid E. coli fasta passes
     :return: None
@@ -54,6 +54,7 @@ def test_integration_valid_file():
     file = os.path.join(TEST_ROOT, 'Data/Escherichia.fna')
     set_input(file)
     ectyper.run_program()
+    assert "Escherichia\tO103\tH2" in caplog.text
 
 
 def test_integration_yersinia(caplog):
@@ -67,7 +68,7 @@ def test_integration_yersinia(caplog):
     assert "Yersinia pestis" in caplog.text
 
 
-def test_valid_fastq_file():
+def test_valid_fastq_file(caplog):
     """
     Given a valid fastq file, get the correct results.
     Use a temp dir for the test output
@@ -76,44 +77,4 @@ def test_valid_fastq_file():
     file = os.path.join(TEST_ROOT, 'Data/Escherichia.fastq')
     set_input(file)
     ectyper.run_program()
-
-
-
-
-
-#     def test_folder_input(self):
-#         input = 'test/Data/test_dir'
-#         output_dir = 'test_folder_input'
-#         set_input(input, output=output_dir)
-#         expected_checksum = 'bd9354495c264bcfb79c5581b9e7c953'
-#         ectyper.run_program()
-#         self.assertEqual(
-#             get_md5(os.path.join('output', output_dir, 'output.csv')),
-#             expected_checksum
-#         )
-#
-#     def test_list_input(self):
-#         input = 'test/Data/test_dir/sample.fasta,test/Data/test_dir/sample.fasta'
-#         output_dir = 'test_list_input'
-#         set_input(input, output=output_dir)
-#         expected_checksum = '3ad690bf9f3ea87dd0200600f72fa618'
-#         ectyper.run_program()
-#         self.assertEqual(
-#             get_md5(os.path.join('output', output_dir, 'output.csv')),
-#             expected_checksum
-#         )
-#
-#     def test_create_tmp_files(self):
-#         # Test relative path
-#         expected_dict = {'assemble_temp_dir': 'test/temp/assemblies',
-#                          'fasta_temp_dir': 'test/temp/fastas',
-#                          'output_dir': os.path.abspath('output') + '/',
-#                          'output_file': os.path.abspath('output/output.csv')}
-#         self.assertDictEqual(expected_dict, ectyper.create_tmp_files(
-#             'test/temp', output_dir=''))
-#         expected_dict = {'assemble_temp_dir': 'test/temp/assemblies',
-#                          'fasta_temp_dir': 'test/temp/fastas',
-#                          'output_dir': os.path.abspath('output/test'),
-#                          'output_file': os.path.abspath('output/test/output.csv')}
-#         self.assertDictEqual(expected_dict, ectyper.create_tmp_files(
-#             'test/temp', output_dir='test'))
+    assert "Escherichia\tO22\tH8" in caplog.text
